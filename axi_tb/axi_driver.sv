393d0e2cce3feac56f791da9fd807ccf794dd159
PriyaNed <anbushanthi.priya@gmail.com>
2026-06-17 16:40:03 +0530

    Update axi_driver.sv

--- axi_tb/axi_driver.sv
+++ axi_tb/axi_driver.sv
@@ -1,1 +1,19 @@
+class axi_driver extends uvm_driver #(axi_transaction);
 
+   virtual axi_if vif;
+
+   task run_phase(uvm_phase phase);
+
+      forever begin
+
+         seq_item_port.get_next_item(req);
+
+         drive_transfer(req);
+
+         seq_item_port.item_done();
+
+      end
+
+   endtask
+
+endclass
